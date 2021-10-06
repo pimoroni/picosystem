d6ea39b13653110a2fa92644900f2744005a76be
Phil Howard <phil@gadgetoid.com>
2021-10-06 17:42:31 +0100

    Sweep some of the boilerplate under the rug
    Moves basic set up of an executable target and inclusion of the PicoSystem library into a new function: picosystem_executable
    
    Wraps the PIXEL_DOUBLE and NO_STARTUP_LOGO compiler flags in friendly functions:
    * pixel_double(<target>)
    * disable_startup_logo(<target>)

--- libraries/picosystem.cmake
+++ libraries/picosystem.cmake
@@ -1,3 +1,6 @@
+# Initialize the Pico SDK
+pico_sdk_init()
+
 add_library(picosystem INTERFACE)
 
 pico_generate_pio_header(picosystem ${CMAKE_CURRENT_LIST_DIR}/screen.pio)
@@ -16,3 +19,25 @@ target_sources(picosystem INTERFACE
 target_include_directories(picosystem INTERFACE ${CMAKE_CURRENT_LIST_DIR})
 
 target_link_libraries(picosystem INTERFACE pico_stdlib hardware_pio hardware_spi hardware_pwm hardware_dma hardware_irq hardware_adc hardware_interp)
+
+function(picosystem_executable NAME SOURCES)
+  add_executable(
+    ${NAME}
+    ${SOURCES}
+    ${ARGN}
+  )
+
+  # Pull in pico libraries that we need
+  target_link_libraries(${NAME} picosystem)
+
+  # create map/bin/hex file etc.
+  pico_add_extra_outputs(${NAME})
+endfunction()
+
+function(pixel_double NAME)
+  target_compile_options(${NAME} PRIVATE -DPIXEL_DOUBLE)
+endfunction()
+
+function(disable_startup_logo NAME)
+  target_compile_options(${NAME} PRIVATE -DNO_STARTUP_LOGO)
+endfunction()
