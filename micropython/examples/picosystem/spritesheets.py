e9bb9571b4a3c4a8574c11df50fedb5526bfce39
Phil Howard <phil@gadgetoid.com>
2021-10-14 20:46:15 +0100

    MicroPython: add 16bpp AAAARRRRGGGGBBBB spritesheets
    Add converted versions of the 32blit spritesheets.
    
    Add spritesheets.py to iterate through and display the spritesheets.
    
    Fix convert-image.py, unbreak, add support for alpha + other QOL fixes.

---
+++ micropython/examples/picosystem/spritesheets.py
@@ -0,0 +1,37 @@
+from picosystem import *
+
+spritesheets = [
+    "s4m_ur4i-dingbads.16bpp",
+    "s4m_ur4i-pirate-characters.16bpp",
+    "s4m_ur4i-pirate-tilemap.16bpp",
+    "s4m_ur4i-platformer.16bpp",
+    "s4m_ur4i-space-shooter-backdrop.16bpp",
+    "s4m_ur4i-space-shooter-ships.16bpp",
+    "s4m_ur4i-top-down-shooter.16bpp"
+]
+
+buffer = Buffer(128, 128)
+spritesheet(buffer)
+last_update = 0
+current_sheet = 0
+
+
+def update(ticks):
+    global last_update, current_sheet
+    if ticks - last_update > 100:
+        last_update = ticks
+        open(spritesheets[current_sheet], "rb").readinto(buffer)
+        current_sheet += 1
+        if current_sheet >= len(spritesheets):
+            current_sheet = 0
+
+
+def draw():
+    pen(0, 0, 0)
+    clear()
+    for x in range(int(120 / 8)):
+        for y in range(int(120 / 8)):
+            sprite(x + y * int(120 / 8), x * 8, y * 8)
+
+
+while True: tick()
