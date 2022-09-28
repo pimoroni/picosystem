907a67dece74e214ba4a5051b9bf8b0c707dc07a
Phil Howard <phil@gadgetoid.com>
2022-09-28 14:48:44 +0100

    MicroPython: boot.py import picosystem to builtins.

--- micropython/modules_py/boot.py
+++ micropython/modules_py/boot.py
@@ -1,7 +1,15 @@
 # PicoSystem boot script
-# Runs the PicoSystem init() and launches the launcher if A is held.
-from picosystem import *
+import picosystem
+import builtins
 
+# Import PicoSystem module constants to builtins,
+# so they are available globally.
+for k, v in picosystem.__dict__.items():
+    if k != "__name__":
+        setattr(builtins, k, v)
+
+
+# Run PicoSystem init() and launches the launcher if A is held.
 init()
 
 if button(A):
