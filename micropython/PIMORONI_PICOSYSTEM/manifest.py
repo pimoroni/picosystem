a63bcfeac37b91c2a8a2ceb218951b27d3709657
Phil Howard <phil@gadgetoid.com>
2023-06-26 09:27:29 +0100

    MicroPython: Update and correct manifest.py.

--- micropython/PIMORONI_PICOSYSTEM/manifest.py
+++ micropython/PIMORONI_PICOSYSTEM/manifest.py
@@ -1,7 +1,2 @@
-freeze("$(PORT_DIR)/modules/")
 freeze("$(BOARD_DIR)/../modules_py/")
-include("$(MPY_DIR)/extmod/uasyncio")
-require("onewire")
-require("ds18x20")
-require("dht")
-require("neopixel")
+include("$(PORT_DIR)/boards/manifest.py")
