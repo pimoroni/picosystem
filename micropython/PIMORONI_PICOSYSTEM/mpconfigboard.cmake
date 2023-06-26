ac97fd7442bdef6591992b2251aff6b733e0a0a3
Phil Howard <phil@gadgetoid.com>
2023-06-26 09:45:19 +0100

    MicroPython: Set MICROPY_C_HEAP_SIZE.

--- micropython/PIMORONI_PICOSYSTEM/mpconfigboard.cmake
+++ micropython/PIMORONI_PICOSYSTEM/mpconfigboard.cmake
@@ -3,3 +3,5 @@ set(MICROPY_BOARD PIMORONI_PICOSYSTEM)
 
 # Board specific version of the frozen manifest
 set(MICROPY_FROZEN_MANIFEST ${CMAKE_CURRENT_LIST_DIR}/manifest.py)
+
+set(MICROPY_C_HEAP_SIZE 4096)
