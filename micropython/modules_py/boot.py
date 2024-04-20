3f6b5a6e3da1dda10457402eaf880669d3681890
Andrew Meyer <andrewm1100@gmail.com>
2024-04-19 18:50:30 -0700

    skip init() on boot to fix possible infinite loop

--- micropython/modules_py/boot.py
+++ micropython/modules_py/boot.py
@@ -1,6 +1,6 @@
 # PicoSystem boot script
 # Runs the PicoSystem init() and launches the launcher if A is held.
-init()
+# init()
 
-if button(A):
-    import launcher  # noqa: F401
+# if button(A):
+#     import launcher  # noqa: F401
